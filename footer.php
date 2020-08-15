fbb1a81c467849881616cf9c7d87ebf0ff04081c
Javier <javipons@mail.com>
2020-08-15 18:19:27 -0300

    added footer

--- footer.php
+++ footer.php
@@ -1,3 +1,19 @@
+        <footer class="footer">
+        <div class="container">
+            <div class="logo-footer d-flex justify-content-center">
+                <img class="logo-footer__imagen" src="<?php echo get_template_directory_uri(); ?>/img/logoblanco.png" alt="Logo Florida"/>
+            </div>
+            <div class="footer-info d-flex justify-content-center flex-column">
+                <div class="footer-info__contacto">
+                    <p>Florida Parque Comercial 2020 | Calle 71 Nº65 - 150 | Tel:520 28 80.</p>
+                </div>
+                <div class="footer-info__copyright">
+                    <p>Todos los derechos reservados. Sitios web 1por1</p>
+                </div>
+            </div>
+        </div>
+        </footer>
     <?php wp_footer(); ?>
+    
     </body>
 </html>
